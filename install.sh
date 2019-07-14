6c9060f1e9b6a2d634eca3cbb1ca7bdf9a22fc2c
wuliaoaaa <yingpengx@gmail.com>
2019-07-14 16:15:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+wget https://raw.githubusercontent.com/wuliaoaaa/wuliaoa.github.io/master/start.sh
 pip install shadowsocks
 password=$1
 echo '{"server":"0.0.0.0","server_port":80,"local_address":"127.0.0.1","local_port":1080,"password":"'${password}'","timeout":300,"method":"aes-256-cfb","fast_open":false}' > ssserver.json
