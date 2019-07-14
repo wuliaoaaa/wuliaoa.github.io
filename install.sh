f414fe8a83b05593e000d9bf8035f2e19236fec8
wuliaoaaa <yingpengx@gmail.com>
2019-07-14 16:07:33 +0800

    run file

---
+++ install.sh
@@ -0,0 +1,9 @@
+pip install shadowsocks
+password=$1
+echo '{"server":"0.0.0.0","server_port":80,"local_address":"127.0.0.1","local_port":1080,"password":"'${password}'","timeout":300,"method":"aes-256-cfb","fast_open":false}' > ssserver.json
+
+if [ ! -f "bbr.sh" ]; then 
+  wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+  chmod +x bbr.sh
+  bash bbr.sh
+fi
---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup ssserver -c /root/ssserver.json -d start &
